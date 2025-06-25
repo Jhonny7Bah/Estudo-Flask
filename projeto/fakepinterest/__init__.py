135629a42b41c7fa1ce158791128c14b57d2d490
GhostEasyX <jhonoliveirapsh@gmail.com>
2025-06-25 07:36:11 -0300

    pausando temporariamente

--- projeto/fakepinterest/__init__.py
+++ projeto/fakepinterest/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask #type:ignore
+from flask_sqlalchemy import SQLAlchemy #type:ignore #importante para banco de dados
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+database = SQLAlchemy(app)
+
+##criação de banco de dados (executar a função quando quiser criar um)
+def criar_banco_de_dados():
+    with app.app_context():
+        database.create_all()
+
 
 from fakepinterest import routes
---
+++ projeto/fakepinterest/models.py
@@ -0,0 +1,1 @@
+from fakepinterest import database
